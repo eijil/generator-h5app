c07015f42c6d12dc567be415f190161348c8353f
李杰 <lijie8@jd.com>
2017-06-07 17:50:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+#generator-h5app
+> Scaffold out a HTML5 web app
+
+## Features
+* enable [ES2015 features](https://babeljs.io/docs/learn-es2015/) using [Babel](https://babeljs.io)
+* using rem layout width [amfe\lib-flexible](https://github.com/amfe/lib-flexible)
+* CSS Autoprefixing
+* preview server with BrowserSync
+* Automagically compile Sass with [libsass](http://libsass.org)
+* Automagically lint your scripts
+
+## Getting Started
+* make sure you have [yo](https://github.com/yeoman/yo) installed: `npm install -g yo`.
+* Install the generator: `npm install -g generator-h5app`.
+* Run yo h5app to scaffold your h5 app.
+* run `npm install` to install dependencies.
+* run `npm start` or `gulp:serve` to preview and watch for changes.
+* run `gulp` to build for production.
+
+## rem
---
+++ app/index.js
@@ -0,0 +1,72 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const yosay = require('yosay');
+const chalk = require('chalk');
+const path = require('path');
+const mkdirp = require('mkdirp');
+
+
+module.exports = class extends Generator {
+
+    initializing() {
+        this.pkg = require('../package.json');
+    }
+
+    prompting() {
+
+        this.log(yosay('\'Allo \'allo! yeoman-generator H5APP.'));
+
+        var prompt = [{
+            type: 'input',
+            name: 'projectName',
+            message: 'you projectName ?',
+            default: this.appname
+        }];
+
+        return this.prompt(prompt).then(answers => {
+            this.projectName = answers.projectName;
+        });
+    }
+
+    writing() {
+        this._writingFiles();
+        this._writingGulpfile();
+        this._writingBower();
+        this._writingPackageJSON();
+    }
+    _writingFiles(){
+      this.fs.copyTpl(
+          this.templatePath('src'),
+          this.destinationPath('src'),
+          this
+      );
+    }
+    _writingGulpfile() {
+        this.fs.copyTpl(
+            this.templatePath('gulpfile.js'),
+            this.destinationPath('gulpfile.js'), {
+                date: (new Date).toISOString().split('T')[0],
+                name: this.pkg.name,
+                version: this.pkg.version
+            }
+        );
+    }
+    _writingBower(){
+      this.fs.copyTpl(
+          this.templatePath('bower_components'),
+          this.destinationPath('bower_components'),
+          this
+      );
+    }
+    _writingPackageJSON(){
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'),
+        this
+      );
+    }
+
+    end() {
+        this.log(chalk.yellow.green('install success!'));
+    }
+};
